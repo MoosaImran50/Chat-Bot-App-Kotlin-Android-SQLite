e8529e564ff6f3e2df432438a45a8b534d8ecf7d
Moosa <moosaimran50@gmail.com>
2023-02-23 00:46:55 +0500

    basic functionality added

--- app/src/main/java/com/example/assignment1/Car.kt
+++
@@ -1,3 +0,0 @@
-package com.example.assignment1
-
-data class Car(val name: String, val manufacturer: String)
---
+++ app/src/main/java/com/example/assignment1/Message.kt
@@ -0,0 +1,3 @@
+package com.example.assignment1
+
+data class Messege(val name: String, val messege: String)
